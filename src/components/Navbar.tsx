c0f691522f3664bea09c494362fa3780922afa75
omar <yourname@example.com>
2024-04-23 01:24:16 +0300

    added feature: display menubar in mobile v using custom hook

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,39 @@
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import useMediaQuery from "@/hooks/useMediaQuery";
+
 const Navbar = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
     <nav className="bg-green-400 text-white">
-      <div className="flex items-center justify-around top-0 z-30 w-full py-6 px-10">
-        <h1 className="logo text-3xl">FitFusion</h1>
-        <div className="flex items-center gap-6 ">
-          <div>Home</div>
-          <div>Benefits</div>
-          <div>Our Classes</div>
-          <div>Contact Us</div>
-        </div>
-        <div className="flex items-center gap-6">
-          <div>Sign In</div>
-          <button className="bg-white text-black py-1 px-3 rounded-[5px] cursor-pointer">
-            Become a Member
+      <div
+        className={`flex items-center ${
+          isAboveMediumScreens ? "justify-around" : "justify-between"
+        } top-0 z-30 w-full py-6 px-10`}
+      >
+        <h1 className={`logo ${isAboveMediumScreens ? "text-3xl" : "text-lg"}`}>
+          FitFusion
+        </h1>
+        {isAboveMediumScreens ? (
+          <>
+            {/* desktop version */}
+            <div className="flex items-center gap-6 ">
+              <div>Home</div>
+              <div>Benefits</div>
+              <div>Our Classes</div>
+              <div>Contact Us</div>
+            </div>
+            <div className="flex items-center gap-6">
+              <div>Sign In</div>
+              <button className="bg-white text-black py-1 px-3 rounded-[5px] cursor-pointer">
+                Become a Member
+              </button>
+            </div>
+          </>
+        ) : (
+          <button>
+            <Bars3Icon className="h-6 w-6" />
           </button>
-        </div>
+        )}
       </div>
     </nav>
   );
