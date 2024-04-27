2a0e816c0afccdaca49d6ef57ce4ea18ec8ca866
omar <yourname@example.com>
2024-04-28 01:35:13 +0300

    add props

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,3 +4,19 @@ export enum SelectedPage {
   OurClasses = "ourclasses",
   ContactUs = "contactus",
 }
+
+export type HomeProps = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+export type LinksProps = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+export type NavbarProps = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+  isTopOfPage: boolean;
+};
