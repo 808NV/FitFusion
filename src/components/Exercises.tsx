bdf7f436ea561f802b81b2f359c6311f1a39ac6e
omar <yourname@example.com>
2024-04-28 04:40:18 +0300

    rename component

---
+++ src/components/Exercises.tsx
@@ -0,0 +1,43 @@
+import { fetchExercises, exerciseOptions } from "@/utils/fetchExercises";
+import { useState } from "react";
+
+const Exercises = () => {
+  const [search, setSearch] = useState("");
+  const [displayRes, setDisplayRes] = useState([]);
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchExercises(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      );
+      const searchedExercises = exercisesData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search.toLowerCase()) ||
+          exercise.bodyPart.toLowerCase().includes(search.toLowerCase()) ||
+          exercise.equipment.toLowerCase().includes(search.toLowerCase()) ||
+          exercise.name.toLowerCase().includes(search.toLowerCase())
+      );
+
+      setSearch("");
+      setDisplayRes(searchedExercises);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Search Exercises</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Search Exercises"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        />
+        <button onClick={() => handleSearch()}>Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default Exercises;
