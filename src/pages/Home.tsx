54547d35b9f3d093c37ea14b91474198500bbaeb
omar <yourname@example.com>
2024-04-28 05:43:09 +0300

    move programs ccontainer to new component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import { SelectedPage, HomeProps } from "@/utils/types";
+import header from "@/assets/header.png";
+import Programs from "@/components/Programs";
+
+const Home = ({ setSelectedPage }: HomeProps) => {
+  return (
+    <div className="bg-black text-white">
+      <header className=" flex flex-col items-center">
+        <img src={header} alt="" className="w-full" />
+      </header>
+
+      <Programs />
+    </div>
+  );
+};
+
+export default Home;
